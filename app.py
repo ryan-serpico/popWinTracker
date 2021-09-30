820a748768e247fe5c3ccbc9256bac93163aacfd
Ryan Serpico <Ryguy360@gmail.com>
2021-09-30 11:02:47 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+url = 'https://www.basketball-reference.com/coaches/popovgr99c.html'
+
+
+def winScraper(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text.encode(
+        'utf-8').decode('ascii', 'ignore'), 'lxml')
+    winTotal = soup.select('.thead > td:nth-child(6)')[0]
+    return int(winTotal.get_text())
+
+
+def textGenerator(currentWinCount):
+    s = '<p> Greg Popovich is <span style = "color: green" > ' + \
+        str(1336 - currentWinCount) + \
+        ' wins </span > away from becoming the all-time winningest coach in NBA history.</p>'
+    return s
+
+
+currentWinCount = winScraper(url)
+text = textGenerator(currentWinCount)
+
+with open('popovichTracker.csv', 'w', encoding='utf-8') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerow([text])
