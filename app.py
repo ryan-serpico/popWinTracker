73b57a7c7e0e13aba8c7b28fca25e36709dc1e5f
Ryan Serpico <Ryguy360@gmail.com>
2021-10-21 15:32:59 -0500

    Centering fix

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ def winScraper(url):
 
 
 def textGenerator(currentWinCount):
-    s = '<p style = "text-align: center"> Greg Popovich is <span style = "color: green" > ' + \
+    s = '<span style = "text-align: center"> Greg Popovich is <span style = "color: green" > ' + \
         str(1336 - currentWinCount) + \
-        ' wins </span > away from becoming the all-time leader in regular season wins for an NBA coach.</p>'
+        ' wins </span > away from becoming the all-time leader in regular season wins for an NBA coach.</span>'
     return s
 
 
